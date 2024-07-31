b7aff4280418389743f80bc0dec5c7db10f26dfc
DaraXOXO <159188265+DaraXOXO@users.noreply.github.com>
2024-07-30 20:39:37 -0400

    Update Github.py

--- Github.py
+++ Github.py
@@ -1,7 +1,4 @@
-echo "# GIT-AND-GITHUB" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/DaraXOXO/GIT-AND-GITHUB.git
-git push -u origin main
+e\cd path/to/your/directory
+
+def say_hello():
+    print("hello")
